4d6ac9b68bffe7b02f7c99e5c55ef1a9713c2ec8
Aman Khan <amankassar790@gmail.com>
2025-10-24 11:12:03 +0530

    install react router dom and setup routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Logo from "../assets/Logo.jpg"
+import React from "react";
+import Logo from "../assets/Logo.jpg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='flex border space-x-8 pl-3 items-center py-4'>
-      <img className='w-[50px] rounded-4xl' src={Logo} alt="" />
-      <a href="/" className='text-blue-400 text-3xl font-bold'>Home</a>
-      <a href="/watchlist" className="text-blue-400 text-3xl font-bold">Watchlist</a>
+    <div className="flex border space-x-8 pl-3 items-center py-4">
+      <img className="w-[50px] rounded-4xl" src={Logo} alt="" />
+      <Link to="/" className="text-blue-400 text-3xl font-bold">
+        Movies
+      </Link>
+      <Link to="/watchlist" className="text-blue-400 text-3xl font-bold">
+        Watchlist
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
