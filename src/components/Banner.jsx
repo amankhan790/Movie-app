01d0ebe460ff18e09c2cd81717cdfe3454e80fed
Aman Khan <amankassar790@gmail.com>
2025-10-29 21:26:58 +0530

    made banner responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import spiderImage from "../assets/1899977.jpg"
+import avenger from "../assets/1899977.jpg"
 
 const Banner = () => {
   return (
+    <>
     <div
-      className="h-[20vh] md:h-[80vh] bg-cover bg-center flex items-end"
+      className="h-[50vh] md:h-[80vh] bg-cover bg-center flex items-end"
       style={{
-        backgroundImage: `url(${spiderImage})`,
+        backgroundImage: `url(${avenger})`,
       }}
     >
-      <div className="bg-blue-900/40 text-center w-full text-white text-xl p-3">
+    </div>
+      <div className="text-center w-full text-white text-xl p-3">
         Avenger Endgame
       </div>
-    </div>
+    </>
   );
 };
 
