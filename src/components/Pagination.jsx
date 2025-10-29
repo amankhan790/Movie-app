a5a1556f7b09a65c9a3c5b16df89c25d201ab00e
Aman Khan <amankassar790@gmail.com>
2025-10-29 22:05:14 +0530

    changed pagination style

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Pagination = ({ pageNo, handlePrev, handleNext }) => {
   return (
-    <div className="bg-gray-400 flex justify-center p-4 mt-8">
-      <div className="px-8 cursor-pointer" onClick={handlePrev}>
-        <i class="ri-arrow-left-circle-fill"></i>
-      </div>
-      <div className="font-bold">{pageNo}</div>
-      <div className="px-8 cursor-pointer" onClick={handleNext}>
-        <i class="ri-arrow-right-circle-fill"></i>
-      </div>
+    <div className="flex justify-center p-4 mt-8 gap-2">
+      {/* <div className="Pagination rounded-2xl"> */}
+        <div className="Pagination px-8 cursor-pointer" onClick={handlePrev}>
+          <i class="Pagination ri-arrow-left-circle-fill"></i>
+        </div>
+        <div className="text-gray-200 font-bold">{pageNo}</div>
+        <div className="Pagination px-8 cursor-pointer" onClick={handleNext}>
+          <i class="Pagination ri-arrow-right-circle-fill"></i>
+        </div>
+      {/* </div> */}
     </div>
   );
 };
